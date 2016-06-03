b557f2b5689a188b8d27bd6a68c09337ad7d4d17
ttuplugdj <romulo.pina7@gmail.com>
2016-06-03 22:31:10 +0000

    Add files via upload

---
+++ rss.php
@@ -0,0 +1,243 @@
+{
+    "colors": {
+        "background": "#000000",
+        "chat": {
+            "ambassador": "#89be6c",
+            "bouncer": "#A7A9FF",
+            "cohost": "#FF7C00",
+            "residentdj": "#84C3F7",
+            "host": "#FF0000",
+            "manager": "#0F81DF",
+            "message": "#B0B0B0"
+        }
+    },
+    "css": {
+        "font": [
+        ],
+        "import": [
+        ],
+        "rule": {
+            ".icon-room": {
+                "background": "url('')",
+                "left": "0px",
+                "top": "0px"
+            },
+
+            "#footer": {
+                "background": "linear-gradient(to bottom,rgba(255, 255, 255, 0.25) 0,rgba(255, 255, 255, 0.06) 100%)"
+            },
+            "#playlist-button": {
+                "background": "rgba(255, 46, 46, 0)"
+            },
+            "#header": {
+                "background": "rgba(10, 10, 10, 1)"
+            },
+            "#store-button": {
+                "background": "rgba(10, 10, 10, 0)"
+            },
+            "#plug-dj": {
+                "background": "rgba(10, 10, 10, 0)"
+            },
+            ".header-panel-button.selected": {
+                "border-top": "#8694b2 3px solid !important",
+                "background": "rgba(10, 10, 10, 1)"
+            },
+            "#plug-menu": {
+                "background": "rgba(10, 10, 10, 1) !important"
+            },
+            "#plug-menu .legal": {
+                "background": "rgba(13, 14, 16, 1) !important"
+            },
+            "#settings": {
+                "background": "rgba(10, 10, 10, 1) !important"
+            },
+            "#settings .header": {
+                "background": "rgba(13, 14, 16, 1) !important"
+            },
+            "#chat": {
+                "background": "rgba(10, 10, 10, 0.8)"
+            },
+            "#dj-button": {
+                "background": "linear-gradient(to bottom, rgba(0, 180, 230, 1) 0%,rgba(0, 156, 221 , 1))"
+            },
+            "#dj-button.is-quit": {
+                "background": "linear-gradient(to bottom, rgba(244, 107, 64, 1) 0%,rgba(240, 79, 48, 1))"
+            },
+            "#dj-button.is-leave": {
+                                "background": "linear-gradient(to bottom, rgba(244, 107, 64, 1) 0%,rgba(240, 79, 48, 01))"
+            },
+            "#chat .emote, #chat .mention, #chat .message, #chat .moderation, #chat .skip, #chat .system, #chat .update, #chat .welcome":{
+                "-webkit-animation":"fadein 0.25s",
+                "-moz-animation":"fadein 0.25s",
+                "-ms-animation":"fadein 0.25s",
+                "-o-animation":"fadein 0.25s",
+                "animation":"fadein 0.25s"
+            },
+            "#chat .message:nth-child(2n+1),#chat .mention:nth-child(2n+1),#chat .skip:nth-child(2n+1),#chat .moderation:nth-child(2n+1),#chat .emote:nth-child(2n+1),#chat .update:nth-child(2n+1),#chat .welcome:nth-child(2n+1)": {
+                "background-color": "rgba(17, 19, 23, 0.32)",
+                "-webkit-animation":"fadein 0.25s",
+                "-moz-animation":"fadein 0.25s",
+                "-ms-animation":"fadein 0.25s",
+                "-o-animation":"fadein 0.25s",
+                "animation":"fadein 0.25s"
+            },
+            "#chat .mention": {
+                "background-color": "rgba(129, 118, 255, 0.22) !important",
+                "font-weight": "700",
+                "padding-left": "25px",
+                "border-left": "#7681FF 3px solid"
+            },
+            "#chat .emote.from-5367337d3e083e609477e1d3": {
+                "border-left": "#ae45bd 3px solid !important",
+                "background-color": "rgba(190, 28, 213, 0.22) !important"
+            },
+            ".emote.from-5367337d3e083e609477e1d3 .from.staff.clickable": {
+                "display": "none"
+            },
+            "#chat .emote.from-5367337d3e083e609477e1d3 .from.staff.clickable": {
+                "color": "#ac76ff !important"
+            },
+            "#chat .emote.from-5367337d3e083e609477e1d3 .text": {
+                "font-style": "normal",
+                "font-weight": "bold",
+                "color": "#ac76ff !important"
+            },
+            "#chat-messages .emote.from-5367337d3e083e609477e1d3 .icon": {
+                "display": "none"
+            },
+            "#chat .emote.from-52cc73b43e083e226a243cb9": {
+                "border-left": "rgba(255, 255, 255, 1) 3px solid !important",
+                "background-color": "rgba(255, 255, 255, 1) !important"
+            },
+            ".emote.from-52cc73b43e083e226a243cb9 .from.staff.clickable": {
+                "display": ""
+            },
+            "#chat .emote.from-52cc73b43e083e226a243cb9 .from.staff.clickable": {
+                "color": "#000000 !important"
+            },
+            "#chat-messages .emote.from-52cc73b43e083e226a243cb9 .icon": {
+                "display": "none"
+            },
+            "#chat .emote.from-52cc73b43e083e226a243cb9 .text": {
+                "font-style": "normal",
+                "font-weight": "bold",
+                "color": "#000000"
+            },
+            "#chat .emote .from": {
+                 "color": ""
+            },
+            "#chat .emote": {
+                "border-left": "",
+                "background-color": ""
+            },
+            "#chat-messages .emote .icon": {
+                "left": ""
+            },
+            "#chat .emote .text": {
+                "font-style": "normal",
+                "font-weight": "",
+                "color": ""
+            },
+            "#footer": {
+                "background": "rgba(10, 10, 10, 0.8)"
+            },
+            "#playlist-button": {
+                "background": "rgba(10, 10, 10, 0)"
+            },
+            ".social-menu .icon-twitter,.social-menu .icon-facebook": {
+                "opacity": "0.1"
+            },
+            "#playlist-panel": {
+                "background": "rgba(10, 10, 10, 1)"
+            },
+            "#playlist-menu": {
+                "background": "rgba(12, 13, 14, 0.6)"
+            },
+            "#search": {
+                "background": "rgba(12, 13, 14, 1)"
+            },
+            "#search-bar": {
+                "background": "rgba(10, 10, 10, 1)"
+            },
+            "#filter-bar": {
+                "background": "rgba(10, 10, 10, 1)"
+            },
+            "#playlist-edit-button": {
+                "background": "rgba(12, 13, 14, 0.7)"
+            },
+            "#playlist-shuffle-button": {
+                "background": "rgba(12, 13, 14, 0.8)"
+            },
+            "#playlist-delete-button": {
+                "background": "rgba(12, 13, 14, 0.9)"
+            },
+            "#user-panel .score": {
+                "background": ""
+            },
+            "#dj-button.is-locked .left,#dj-button.is-full .left,#vote .crowd-response.disabled .bottom": {
+                "background": "rgba(10, 10, 10, 1)"
+            },
+            "#dj-button.is-quit .left,#dj-button.is-leave .left,#playlist-delete-button:hover": {
+                "background": "linear-gradient(to bottom, rgba(244, 107, 64, 1) 0%,rgba(240, 79, 48, 1))"
+            },
+            "#chat .mention.from-.from-ambassador > i": {
+                "background-image": "url('') !important",
+                "background-position": "0 0"
+            },
+            "#chat .message.from-.from-ambassador > i": {
+                "background-image": "url('') !important",
+                "background-position": "0 0"
+            },
+            "#chat .emote.from-.from-ambassador > i": {
+                "background-image": "url('') !important",
+                "background-position": "0 0"
+            },
+            "@keyframes fadein":{
+               "from {opacity":"0;}",
+               "to {opacity":"1;}"
+            },
+            "@-moz-keyframes fadein":{
+               "from {opacity":"0;}",
+               "to {opacity":"1;}"
+            },
+            "@-webkit-keyframes fadein":{
+               "from {opacity":"0;}",
+               "to {opacity":"1;}"
+            },
+            "@-ms-keyframes fadein":{
+               "from {opacity":"0;}",
+               "to {opacity":"1;}"
+            },
+            "@-o-keyframes fadein":{
+               "from {opacity":"0;}",
+               "to {opacity":"1;}"
+            },
+            "#user-lists .list":{
+               "-webkit-animation":"fadein 0.5s",
+               "-moz-animation":"fadein 0.5s",
+               "-ms-animation":"fadein 0.5s",
+               "-o-animation":"fadein 0.5s",
+               "animation":"fadein 0.5s"
+            },
+            "#waitlist .list":{
+               "-webkit-animation":"fadein 0.5s",
+               "-moz-animation":"fadein 0.5s",
+               "-ms-animation":"fadein 0.5s",
+               "-o-animation":"fadein 0.5s",
+               "animation":"fadein 0.5s"
+            },
+            "#plugcubed":{
+               "background": "rgba(10, 10, 10, 0) !important"
+            }
+        }
+    },
+    "rules": {
+        "allowAutowoot":        true,
+        "allowAutojoin":        true,
+        "allowAutograb":        true,
+        "allowSmartVote":       true,
+        "allowEmotes":          true,
+        "allowShowingMehs":     true,
+        "forceSmartVote":       false
+    }
+}
